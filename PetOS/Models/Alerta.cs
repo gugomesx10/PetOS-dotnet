e746158b2aee0e58c47a9606f67b0f9516eee0e4
gugomesx10 <gustavogomesmartins4@icloud.com>
2026-05-03 12:57:15 -0400

    feat: implementar API REST PetOS com Oracle EF Core e Swagger

---
+++ PetOS/Models/Alerta.cs
@@ -0,0 +1,37 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace PetOS.Models;
+
+public class Alerta
+{
+    public int Id { get; set; }
+
+    [Required]
+    public int PetId { get; set; }
+
+    public int? RotinaId { get; set; }
+
+    [Required]
+    [MaxLength(120)]
+    public string Titulo { get; set; } = string.Empty;
+
+    [Required]
+    [MaxLength(600)]
+    public string Mensagem { get; set; } = string.Empty;
+
+    public DateTime DataAlerta { get; set; }
+
+    public AlertaStatus Status { get; set; } = AlertaStatus.Pendente;
+
+    public Pet Pet { get; set; } = null!;
+    public Rotina? Rotina { get; set; }
+}
+
+public enum AlertaStatus
+{
+    Pendente = 1,
+    Enviado = 2,
+    Lido = 3,
+    Cancelado = 4
+}
+
