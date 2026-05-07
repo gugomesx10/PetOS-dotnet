a45437aa0e16982fa6343be1b7f8a32c613e9ebc
gugomesx10 <gustavogomesmartins4@icloud.com>
2026-05-07 11:11:54 -0400

    feat: Refazendo o Program.cs, e agora irei começar criar as classes Models

--- PetOS/Program.cs
+++ PetOS/Program.cs
@@ -1,44 +1,29 @@
 using Microsoft.EntityFrameworkCore;
-using PetOS.Data;
-using PetOS.Exceptions;
-using PetOS.Repositories;
-using PetOS.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
-builder.Services.AddControllers();
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
-
-builder.Services.AddDbContext<AppDbContext>(options =>
-{
-    var connectionString = builder.Configuration.GetConnectionString("OracleDb")
-                           ?? throw new InvalidOperationException("Connection string 'OracleDb' nao configurada.");
-    options.UseOracle(connectionString);
+builder.Services.AddDbContext<ApplicationContext>(options => {
+    options.UseOracle(builder.Configuration.GetConnectionString("Oracle"));
 });
 
-builder.Services.AddScoped<IPetRepository, PetRepository>();
-builder.Services.AddScoped<IVacinaRepository, VacinaRepository>();
-builder.Services.AddScoped<IRotinaRepository, RotinaRepository>();
-builder.Services.AddScoped<IAlertaRepository, AlertaRepository>();
-
-builder.Services.AddScoped<IPetService, PetService>();
-builder.Services.AddScoped<IVacinaService, VacinaService>();
-builder.Services.AddScoped<IRotinaService, RotinaService>();
-builder.Services.AddScoped<IAlertaService, AlertaService>();
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen(c => { 
+    c.EnableAnnotations();
+});
 
 var app = builder.Build();
 
-app.UseMiddleware<ApiExceptionMiddleware>();
-
+// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
 }
 
+app.UseAuthorization();
 
-app.UseHttpsRedirection();
 app.MapControllers();
 
 app.Run();
